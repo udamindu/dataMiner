9e7236d43b152cb8c131a1cd93e990590b49a2c6
UDamindu <daminduuthpala@gmail.com>
2016-08-20 14:41:24 +0530

    basic jframe added

--- ZooDataMiner/src/zoodataminer/ZooDataMiner.java
+++ ZooDataMiner/src/zoodataminer/ZooDataMiner.java
@@ -5,6 +5,8 @@
  */
 package zoodataminer;
 
+import GUI.ZooDataClassification;
+
 /**
  *
  * @author Arunalu Hasakelum
@@ -16,6 +18,7 @@ public class ZooDataMiner {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ZooDataClassification z = new ZooDataClassification();
     }
     
 }
